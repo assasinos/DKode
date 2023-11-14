c501aac0d18c6df9cf44b3d176ca03fb128acfd7
assasinos <kubasem198@gmail.com>
2023-11-14 11:26:17 +0100

    Add Domain To Api mappings

---
+++ BackEndPracticalTest/FactoryAPI/Mapping/DomainToApiContractMapper.cs
@@ -0,0 +1,32 @@
+﻿using FactoryAPI.Contracts.Responses;
+using FactoryAPI.Models;
+
+namespace FactoryAPI.Mapping
+{
+    public static class DomainToApiContractMapper
+    {
+        public static ItemResponse ToItemResponse(this Item item)
+        {
+            return new ItemResponse
+            {
+                Id = item.Id,
+                Description = item.Description,
+                Name = item.Name,
+                Price = item.Price,
+            };
+        }
+        public static GetAllItemsResponse ToItemsResponse(this IEnumerable<Item> items)
+        {
+            return new GetAllItemsResponse
+            {
+                Items = items.Select(x => new ItemResponse
+                {
+                    Id = x.Id,
+                    Description = x.Description,
+                    Name = x.Name,
+                    Price = x.Price
+                })
+            };
+        }
+    }
+}
