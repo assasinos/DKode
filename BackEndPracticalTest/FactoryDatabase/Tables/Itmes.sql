c125a303b2c38504ce1dde51b0ab7babd399c401
assasinos <kubasem198@gmail.com>
2023-11-14 10:41:51 +0100

    Create Database

---
+++ BackEndPracticalTest/FactoryDatabase/Tables/Itmes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Items](
+	[ID] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)
