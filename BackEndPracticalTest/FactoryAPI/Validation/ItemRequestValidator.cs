d4afb3081b330731425eea7c0d74e6764ac13bea
assasinos <kubasem198@gmail.com>
2023-11-14 12:05:41 +0100

    Add ItemRequest Validation

---
+++ BackEndPracticalTest/FactoryAPI/Validation/ItemRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FactoryAPI.Contracts.Requests;
+using FluentValidation;
+
+namespace FactoryAPI.Validation
+{
+    public class ItemRequestValidator : AbstractValidator<ItemRequest>
+    {
+        public ItemRequestValidator()
+        {
+            //00.01 => 999999999999999999.99
+            RuleFor(x => x.Price).PrecisionScale(18,2,false);
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.Description).NotNull();
+        }
+    }
+}
