4f5b1ee64f69e6bb4d43ca5a6533ec11fdb5da5c
assasinos <kubasem198@gmail.com>
2023-11-14 11:21:43 +0100

    Add Response contracts

---
+++ BackEndPracticalTest/FactoryAPI/Contracts/Responses/GetAllItemsResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace FactoryAPI.Contracts.Responses
+{
+    public class GetAllItemsResponse
+    {
+        public IEnumerable<ItemResponse> Items { get; init; } = Enumerable.Empty<ItemResponse>();
+    }
+}
---
+++ BackEndPracticalTest/FactoryAPI/Contracts/Responses/ItemResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace FactoryAPI.Contracts.Responses
+{
+    public class ItemResponse
+    {
+        public Guid Id { get; init; }
+
+        public string Name { get; init; } = default!;
+
+        public string Description { get; init; } = default!;
+
+        public decimal Price { get; init; } = default!;
+    }
+}
